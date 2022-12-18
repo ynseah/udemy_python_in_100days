0210262eb2298e46a77c4378964dc87ca792418d
YN Seah <yeowngee@hotmail.com>
2022-12-19 06:30:29 +0800

    day 9

---
+++ day-9-start/main.py
@@ -0,0 +1,9 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
+print (programming_dictionary["Bug"])
+
+for item in programming_dictionary:
+    print (item)
