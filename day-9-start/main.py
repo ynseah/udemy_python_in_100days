38223cda8fc866de52760c1e6b385a3dd5fcd91d
YN Seah <yeowngee@hotmail.com>
2022-12-19 06:47:47 +0800

    day 9 dict

--- day-9-start/main.py
+++ day-9-start/main.py
@@ -7,3 +7,29 @@ print (programming_dictionary["Bug"])
 
 for item in programming_dictionary:
     print (item)
+
+#Nesting dictionary in a disctionary
+travel_log = {
+    "France": {
+        "cities_visited": ["Paris","Lille","Dijon"],  
+        "total_visits": 12
+        },
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+
+}
+
+#Nesting dictionary in aList
+
+travel_log = [
+    {
+        "country": "France",         
+        "cities_visited": ["Paris","Lille","Dijon"],  
+        "total_visits": 12,
+    },
+    {
+        "country": "Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"]
+        "total_visits": 3,
+    },
+
+]
